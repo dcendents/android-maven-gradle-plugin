f5a16791e9e8d2b9131a666a21ccbd7a3c162e5e
Daniel Beland <dcendents@gmail.com>
2016-08-15 21:59:15 -0400

    using built-in GradleRunner instead of custom gradletest plugin

---
+++ src/test/groovy/org/gradle/api/plugins/AndroidMavenPluginIT.groovy
@@ -0,0 +1,129 @@
+package org.gradle.api.plugins
+
+import org.gradle.testkit.runner.GradleRunner
+import static org.gradle.testkit.runner.TaskOutcome.*
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+import spock.lang.Shared
+import spock.lang.Unroll
+
+class AndroidMavenPluginIT extends Specification {
+	
+	@Shared def gradleVersions = "${System.properties['gradle.versions']}".split(",")
+	@Shared def androidGradleBuildVersion = "${System.properties['android.gradle.build.version']}"
+	@Shared def androidCompileSdkVersion = "${System.properties['android.compile.sdk.version']}"
+	@Shared def androidBuildToolsVersion = "${System.properties['android.build.tools.version']}"
+	
+    @Rule final TemporaryFolder testProjectDir = new TemporaryFolder()
+	
+	File androidManifest
+	File settingsFile
+    File buildFile
+	
+    def setup() {
+		def srcFolder = testProjectDir.newFolder('src')
+		def mainFolder = new File(srcFolder, 'main')
+		mainFolder.mkdirs()
+		androidManifest = new File(mainFolder, 'AndroidManifest.xml')
+        settingsFile = testProjectDir.newFile('settings.gradle')
+        buildFile = testProjectDir.newFile('build.gradle')
+    }
+
+	@Unroll
+    def "pom is installed with gradle #gradleVersion"() {
+        given:
+		androidManifest << """<?xml version="1.0" encoding="utf-8"?>
+			<manifest xmlns:android="http://schemas.android.com/apk/res/android" package="org.test.simple" android:versionCode="1" android:versionName="1.0" >
+				<application />
+			</manifest>
+		"""
+		
+		settingsFile << "rootProject.name = 'simple'"
+		
+		def uri = testProjectDir.root.toURI()
+        buildFile << """
+			buildscript {
+				System.properties['com.android.build.gradle.overrideVersionCheck'] = 'true'
+				
+				repositories {
+					jcenter()
+					mavenCentral()
+					mavenLocal()
+				}
+
+				dependencies {
+					classpath "com.android.tools.build:gradle:${androidGradleBuildVersion}"
+				}
+			}
+			
+			plugins {
+				id "com.github.dcendents.android-maven"
+			}
+
+			apply plugin: 'com.android.library'
+
+			group = 'org.test'
+			version = '1.0'
+			
+			install {
+				repositories {
+					// Should be mavenInstaller but cannot find how to override the location
+					mavenDeployer {
+						repository(url: "${uri}/${gradleVersion}/repo")
+					}
+				}
+			}
+
+			android {
+				compileSdkVersion "${androidCompileSdkVersion}"
+				buildToolsVersion "${androidBuildToolsVersion}"
+			}
+
+			task runGradleTest {
+				dependsOn install
+			}
+        """
+		
+        when:
+        def result = GradleRunner.create()
+			.withGradleVersion(gradleVersion)
+            .withProjectDir(testProjectDir.root)
+            .withArguments('install')
+			.withPluginClasspath()
+            .build()
+
+        then:
+        result.task(":install").outcome == SUCCESS
+		validateRepo(gradleVersion);
+		
+        where:
+        gradleVersion << gradleVersions
+    }
+	
+	private void validateRepo(String version) {
+		File repo = new File(testProjectDir.root, version + "/repo/org/test/simple")
+		assert repo.exists()
+		
+		File metadata = new File(repo, "maven-metadata.xml");
+		File metadataMd5 = new File(repo, "maven-metadata.xml.md5");
+		File metadataSha1 = new File(repo, "maven-metadata.xml.sha1");
+		assert metadata.exists()
+		assert metadataMd5.exists()
+		assert metadataSha1.exists()
+		
+		File aar = new File(repo, "1.0/simple-1.0.aar");
+		File aarMd5 = new File(repo, "1.0/simple-1.0.aar.md5");
+		File aarSha1 = new File(repo, "1.0/simple-1.0.aar.sha1");
+		assert aar.exists()
+		assert aarMd5.exists()
+		assert aarSha1.exists()
+		
+		File pom = new File(repo, "1.0/simple-1.0.pom");
+		File pomMd5 = new File(repo, "1.0/simple-1.0.pom.md5");
+		File pomSha1 = new File(repo, "1.0/simple-1.0.pom.sha1");
+		assert pom.exists()
+		assert pomMd5.exists()
+		assert pomSha1.exists()
+	}
+}
